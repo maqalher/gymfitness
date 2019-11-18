38302a286c413a3c5223a8ff5de959805ca1a509
maqalher <maqalher@hotmail.com>
2019-11-18 00:43:12 -0600

    add style pagina

--- wp-content/themes/gymfitness/page.php
+++ wp-content/themes/gymfitness/page.php
@@ -1,9 +1,22 @@
 <?php get_header(); ?>
 
-<?php while( have_posts() ): the_post()  ?>
-    <h1><?php the_title();?></h1>
-    <p><?php the_content();?></p>   
-<?php endwhile?>
+    <main class="contenedor pagina seccion con-sidebar">
+        <div class="contenido-principal">
+            <?php while( have_posts() ): the_post()  ?>
+                <h1 class="text-center text-primario"><?php the_title();?></h1>
+
+                <?php 
+                    if(has_post_thumbnail() ):
+                        the_post_thumbnail('blog', array('class' => 'imagen-destacada')); 
+                    endif;        
+                ?>
+
+                <p><?php the_content();?></p>   
+            <?php endwhile?>
+        </div>
+        
+        <?php get_sidebar();?>
+    </main>
 
 
 <?php get_footer(); ?>
---
+++ wp-content/themes/gymfitness/sidebar.php
@@ -0,0 +1,3 @@
+<aside class="sidebar">
+    <h1 class="text-primario text-center">sidebar aqui</h1>
+</aside>
